7834f9f958b5ac93cfcd77ed01ea20b1b648589a
Denis Ulate <92893770+DenisUlate@users.noreply.github.com>
2025-03-20 16:44:03 -0600

    add sidebar component, local fonts, and skeleton UI elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
 	return (
 		<div>
-			<h1>Home</h1>
+			<h1 className="font-mono text-2xl font-bold">Home</h1>
+			<h1 className="font-sans text-2xl font-bold">Home</h1>
 		</div>
 	);
 }
