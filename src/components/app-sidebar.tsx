46eb9572db81dee38c4870c90f21c2ad859ac849
Denis Ulate <92893770+DenisUlate@users.noreply.github.com>
2025-03-20 22:06:42 -0600

    update sidebar component with new icons, categories, and logo

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,6 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Tags, Home, LayoutGrid, Archive, Info } from "lucide-react";
+import Image from "next/image";
+import logoImage from "../../public/imagenes/logo/logo_1.webp";
 
 import {
 	Sidebar,
@@ -19,41 +21,57 @@ const items = [
 		icon: Home,
 	},
 	{
-		title: "Inbox",
+		title: "Categories",
 		url: "#",
-		icon: Inbox,
+		icon: LayoutGrid,
 	},
 	{
-		title: "Calendar",
+		title: "Tags",
 		url: "#",
-		icon: Calendar,
+		icon: Tags,
 	},
 	{
-		title: "Search",
+		title: "Archives",
 		url: "#",
-		icon: Search,
+		icon: Archive,
 	},
 	{
-		title: "Settings",
+		title: "About",
 		url: "#",
-		icon: Settings,
+		icon: Info,
 	},
 ];
 
 export function AppSidebar() {
 	return (
-		<Sidebar>
+		<Sidebar className="py-7 px-4">
 			<SidebarContent>
 				<SidebarGroup>
-					<SidebarGroupLabel>Application</SidebarGroupLabel>
-					<SidebarGroupContent>
+					<div className="space-y-4">
+						<div className="border-2 border-gray-300 bg-slate-700 flex items-center justify-center w-28 h-28 rounded-full">
+							<Image
+								src={logoImage}
+								alt="Logo"
+								width={50}
+								height={50}
+							/>
+						</div>
+						<h1 className="font-bold text-3xl">Denis</h1>
+					</div>
+					<SidebarGroupContent className="mt-8">
 						<SidebarMenu>
 							{items.map((item) => (
-								<SidebarMenuItem key={item.title}>
-									<SidebarMenuButton asChild>
+								<SidebarMenuItem
+									key={item.title}
+									className="p-2">
+									<SidebarMenuButton
+										asChild
+										className="hover:bg-slate-100 hover:text-slate-500">
 										<a href={item.url}>
 											<item.icon />
-											<span>{item.title}</span>
+											<span className="uppercase font-semibold">
+												{item.title}
+											</span>
 										</a>
 									</SidebarMenuButton>
 								</SidebarMenuItem>
