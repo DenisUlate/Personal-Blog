9d782739e4734edd12d92f90ce3d06fd4daf9e18
Denis Ulate <92893770+DenisUlate@users.noreply.github.com>
2025-07-03 20:47:45 -0600

    Update: enhance next.config.ts with image remote patterns and experimental settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "dummyjson.com",
+			},
+			{
+				protocol: "https",
+				hostname: "picsum.photos",
+			},
+		],
+	},
+	experimental: {
+		optimizeCss: true,
+		turbo: {
+			rules: {
+				"*.svg": {
+					loaders: ["@svgr/webpack"],
+					as: "*.js",
+				},
+			},
+		},
+	},
+	compiler: {
+		removeConsole: process.env.NODE_ENV === "production",
+	},
+	poweredByHeader: false,
+	reactStrictMode: true,
 };
 
 export default nextConfig;
